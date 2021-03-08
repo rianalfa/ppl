28f9625823e72a4374da99801780c8ba0c3f808d
Yudistira Elton Jhon <221810670@stis.ac.id>
2021-03-08 09:04:06 +0700

    apalah

--- application/views/umum.php
+++ application/views/umum.php
@@ -1,49 +1,160 @@
-<!DOCTYPE html>
+<!doctype html>
+
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type='text/css' href=" <?= base_url("css/style.css") ?> ">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- Font-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
-    <link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <title>Kalkulator Statistik</title>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+
+    <title>Kalkulator Statistik Portable</title>
 </head>
 
 <body>
-    <!-- Welcome Section  -->
-    <section id="welcome">
-        <div class="welcome container">
-            <div>
-                <h1>Kalkulator<span></span></h1>
-                <h1>Statistik <span></span></h1>
-                <h1>Portable <span></span></h1>
-                <a href="<?= base_url("calculator") ?>" type="button" class="cta">Mulai</a>
+    <div id="wrapper">
+        <div class="overlay"></div>
+        <!-- Sidebar -->
+        <nav class="fixed-top align-top" id="sidebar-wrapper" role="navigation">
+            <div class="simplebar-content" style="padding: 0px;">
+                <a class="sidebar-brand" href="index.html">
+                    <span class="align-middle">Menu</span>
+                </a>
+                <ul class="navbar-nav align-self-stretch">
+                    <li class="">
+                        <a class="nav-link text-left active" role="button" aria-haspopup="true" aria-expanded="false">
+                            <i class="flaticon-bar-chart-1"></i> Halaman Utama
+                        </a>
+                    </li>
+                    <li class="">
+                        <a class="nav-link text-left" role="button">
+                            <i class="flaticon-bar-chart-1"></i> Kalkulator
+                        </a>
+                    </li>
+                    <li class="has-sub">
+                        <a class="nav-link collapsed text-left" href="#collapseExample2" role="button" data-toggle="collapse">
+                            <i class="flaticon-user"></i> Statistik
+                        </a>
+                        <div class="collapse menu mega-dropdown" id="collapseExample2">
+                            <div class="dropmenu" aria-labelledby="navbarDropdown">
+                                <div class="container-fluid ">
+                                    <div class="row">
+                                        <div class="col-lg-12 px-2">
+                                            <div class="submenu-box">
+                                                <ul class="list-unstyled m-0">
+                                                    <li><a href="">ANOVA</a></li>
+                                                    <li><a href="">Averages</a></li>
+                                                    <li><a href="">Circular</a></li>
+                                                    <li><a href="">Deskriptif</a></li>
+                                                    <li><a href="">Distance</a></li>
+                                                    <li><a href="">Distribusi</a></li>
+                                                    <li><a href="">Percobaan</a></li>
+                                                    <li><a href="">Korelasi</a></li>
+                                                    <li><a href="">Outlier</a></li>
+                                                    <li><a href="">Variabel Acak</a></li>
+                                                    <li><a href="">Regresi</a></li>
+                                                    <li><a href="">Uji Signifikansi</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- /#sidebar-wrapper -->
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div id="content">
+                <div class="container-fluid p-0 px-lg-0 px-md-0">
+                    <!-- Topbar -->
+                    <nav class="navbar navbar-expand navbar-light my-navbar">
+                        <!-- Sidebar Toggle (Topbar) -->
+                        <div type="button" id="bar" class="nav-icon1 hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <!-- Topbar Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Nav Item -->
+                            <li class="nav-item d-sm-none">
+                                <h3>
+                                    Kalkulator Statistik Portabel
+                                </h3>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- End of Topbar -->
+                    <!-- Begin Page Content -->
+                    <div class="container-fluid px-lg-4">
+                        <div class="row">
+                            <div class="col-md-12 mt-lg-4 mt-4">
+                                <!-- Page Heading -->
+                                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                                    <h1 class="h3 mb-0 text-gray-800">Tentang</h1>
+                                </div>
+                                <div class="col-md-12 mt-4">
+                                    <div class="card">
+                                        <h5>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                                            survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                        </h5>
+                                        <h5>
+                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                                            survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                                            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                        </h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row text-muted">
+                            <div class="col-6 text-left">
+                                <p class="mb-0">
+                                    &copy <strong>Kelompok 2 PPL 2020/2021</strong>
+                                </p>
+                            </div>
+                            <div class="col-6 text-right">
+                                <p class="mb-0">
+                                    <a href="https://www.stis.ac.id" class="text-muted" target="_blank"><strong>Politeknik Statistika STIS</strong></a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+
             </div>
         </div>
-    </section>
-    <!-- End welcome Section  -->
+        <!-- /#page-content-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script>
+        $('#bar').click(function() {
+            $(this).toggleClass('open');
+            $('#page-content-wrapper ,#sidebar-wrapper').toggleClass('toggled');
 
-    <!-- Calculator section -->
-    <section id="calculator">
-        <input type="checkbox" id="check">
-        <label for="check">
-            <i class="fas fa-bars" id="btn"></i>
-            <i class="fas fa-times" id="cancel"></i>
-        </label>
-        <div class="sidebar">
-            <header>Kalkulator Statistik</header>
-            <ul>
-                <li><a href=" <?= base_url("calculator") ?> ">Calkulator</a></li>
-                <li><a href="#">Statistik</a></li>
-                <li><a href="regression">Regression</a></li>
-            </ul>
-        </div>
-    </section>
-    <!-- End calculator section-->
-
+        });
+    </script>
 </body>
 
 </html>
