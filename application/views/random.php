9c1f72947f1b6178ee3ba48fbbb747511a39d036
anlisa <221810177@stis.ac.id>
2021-03-20 21:11:35 +0700

    coba random

---
+++ application/views/random.php
@@ -0,0 +1,69 @@
+<!doctype html>
+
+<html lang="en">
+
+<?php $this->load->view('_partials/head'); ?>
+
+<body>
+    <div id="wrapper">
+        <div class="overlay"></div>
+
+        <!-- Sidebar -->
+        <?php $this->load->view('_partials/sidebar'); ?>
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper" class="toggled">
+            <div id="content">
+                <div class="container-fluid p-0 px-lg-0 px-md-0">
+
+                    <!-- Navbar -->
+                    <?php $this->load->view('_partials/navbar'); ?>
+
+                    <!-- Begin Page Content -->
+                    <div class="container-fluid px-lg-4">
+                        <div class="row">
+                            <div class="col-md-12 mt-lg-4 mt-4">
+                                <!-- Page Heading -->
+                                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                                    <h1 class="h3 mb-0 text-gray-800">Peubah Acak</h1>
+                                </div>
+                                <div class="col-md-12 mt-4" id="mainContainer">
+                                    <div class="card mb-2">
+                                        <h5>
+                                            Variabel Acah atau Peubah Acak adalah variabel numerik yang nilai spesifiknya tidak dapat diprediksi dengan pasti sebelum dilakukan eksperimen. 
+                                            Suatu peubah acak didefinisikan sebagai fungsi yang memetakan suatu kejadian pada sesuatu interval bilangan riil. 
+                                        </h5>
+                                        <div class="d-flex justify-content-left">
+                                            <button class="btn btn-secondary ml-2 mb-2" onclick="gettingStarted();">Getting Started</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Footer -->
+                <?php $this->load->view('_partials/footer'); ?>
+
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script>
+        $('#bar').click(function() {
+            $(this).toggleClass('open');
+            $('#page-content-wrapper ,#sidebar-wrapper').toggleClass('toggled');
+
+        });
+        
+    </script>
+    <?php $this->load->view('js/random'); ?>
+</body>
+
+</html>
+
